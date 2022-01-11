5a4f2f67b49cbbbd6c4dff799601ff210c8f9d7f
MCprotein <chss3339@gmail.com>
2022-01-11 20:24:53 +0900

    백엔드: 자바 스프링, 프론트엔드: 자바스크립트, css, html

---
+++ chatapp/src/main/java/com/cos/chatapp/Chat.java
@@ -0,0 +1,21 @@
+package com.cos.chatapp;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "chat") // collection 이름 설정
+public class Chat {
+
+    @Id
+    private String id;
+    private String msg;
+    private String sender; // 보내는 사람
+    private String receiver; // 받는 사람 (귓속말)
+    private Integer roomNum; // 방 번호
+
+    private LocalDateTime createdAt;
+}
---
+++ chatapp/src/main/java/com/cos/chatapp/ChatController.java
@@ -0,0 +1,38 @@
+package com.cos.chatapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController // 데이터 리턴 서버
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    @CrossOrigin
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
+        return chatRepository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+ 
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return chatRepository.mFindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat) { // Mono는 데이터를 한번만 리턴한다. 계속할거면 Flux
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (Message Converter)
+    }
+}
